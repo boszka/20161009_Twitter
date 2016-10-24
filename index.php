e124ed181087eae3c2aec5fa4e19da68b0cced3a
boszka <larika@wp.pl>
2016-10-25 00:46:10 +0200

    poprawki w index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 session_start();
 if (!isset($_SESSION['loggedUserId'])) {
     header('Location: login.php');
-    
 }
 ?>
 
@@ -10,25 +9,22 @@ if (!isset($_SESSION['loggedUserId'])) {
     <head></head>
     <body>
         <?php
-        
-        
+        require_once 'src/connection.php';
+        require_once 'src/User.php';
         if (isset($_SESSION['loggedUserId'])) {
-            require_once 'src/connection.php';
-            require_once 'src/User.php';
+
             $loadedUser = User::loadUserById($conn, 12);
             //$loadedUser = User::loadUserById($conn, $_SESSION['loggedUserId']);
-            
+        
+        echo 'Uzytkowniku ' . $loadedUser->getUsername() . ', witamy na twitterze!';
+        echo '<br>';
 
-            echo 'Uzytkowniku ' . $loadedUser->getUsername() . ', witamy na twitterze!';
-            echo '<br>';
 
-
-            echo '<br>';
-            echo '<a href="logout.php">wyloguj sie</a>';
-            echo '<br>';
-            echo '<a href=edit.php>twoj profil</a>';
-            echo '<br>';
-            //echo '<a href=edit.php?userId=' . $newUserId . '>twoj profil</a>';
+        echo '<br>';
+        echo '<a href="logout.php">wyloguj sie</a>';
+        echo '<br>';
+        echo '<a href=edit.php>twoj profil</a>';
+        echo '<br>';
         }
         ?>
         <br>
